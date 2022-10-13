84ae85523aae594a7a41bc9ff5745e64174a0764
Vlad Rindevich <vladrin@vaadin.com>
2022-10-13 20:32:52 +0300

    refactor: cleanup [2]

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,6 @@
-import React from 'react';
+import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 
-createRoot(document.getElementById('outlet')!).render(React.createElement(RouterProvider, { router }));
+createRoot(document.getElementById('outlet')!).render(createElement(RouterProvider, { router }));
